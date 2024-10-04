5cd56f42dc5de20492c1c12b4a08d2a845702a9f
Hanse Kim <hanse0910@gmail.com>
2024-10-04 13:57:43 +0900

    update

--- 24q2-kps/model.py
+++ 24q2-kps/model.py
@@ -12,7 +12,7 @@ class CommonModule(nn.Module):
         self.mlp_dim = mlp_dim
         self.batch_dim = batch_dim
         # mha specs
-        self.head_dim = head_dim
+        self.head_dim = head_dim # should use key_dim and vec_dim separately
         self.head_num = head_num
 
         self.device = device
@@ -21,18 +21,18 @@ class CommonModule(nn.Module):
 
     def compat(self, Wq, Wk, Wv, vec):
         qv, kv, vv = ( # vectors : batch dim x node dim x head num x head dim
-        torch.einsum('kij,abk->abij', Wq, vec),
-        torch.einsum('kij,abk->abij', Wk, vec),
-        torch.einsum('kij,abk->abij', Wv, vec))
+        torch.einsum('xij,abx->abij', Wq, vec),
+        torch.einsum('xij,abx->abij', Wk, vec),
+        torch.einsum('xij,abx->abij', Wv, vec))
         
-        compat = torch.einsum('aibk,ajbk->aijb',qv,kv)/np.sqrt(self.head_dim) # compat : batch dim x node dim x node dim x head num
+        compat = torch.einsum('aibx,ajbx->aijb',qv,kv)/np.sqrt(self.head_dim) # compat : batch dim x node dim x node dim x head num
 
         return compat, vv
 
     def attention(self, compat, vv, Wo):
         weight = F.softmax(compat, dim = 2) # weight : batch dim x node dim x node dim x head num
-        received_vec = torch.einsum('aijb,ajkb->aikb', weight, vv) # recvec : batch dim x node dim x head num x head dim
-        mha = torch.einsum('ijk,alij->alk', Wo, received_vec) # mha : batch dim x node dim x unif dim
+        received_vec = torch.einsum('aixb,axbj->aibj', weight, vv) # recvec : batch dim x node dim x head num x head dim
+        mha = torch.einsum('xyj,aixy->aij', Wo, received_vec) # mha : batch dim x node dim x unif dim
         return mha 
 
 class Encoder(CommonModule):
@@ -69,7 +69,7 @@ class Encoder(CommonModule):
             mha_l = self.attention(compat_mask, vv_l, self.ov_p[l])
 
             hhat_l = self.bn1[l](hvec_l + mha_l)
-            hvec_l = self.bn2[l](hhat_l + self.mha_mlp[l](hhat_l))
+            hvec_l = self.bn2[l](hhat_l + self.mha_mlp[l](hhat_l)) # hvec : batch dim x node dim x unif dim
 
         hbar_l = torch.sum(hvec_l, dim = 1)/(1+self.market_dim) # hbar : batch dim x unif dim
 
@@ -85,15 +85,19 @@ class PathModule(CommonModule):
         self.kv_p = nn.Parameter(torch.rand((self.unif_dim, self.head_num, self.head_dim), device = self.device)) 
         self.vv_p = nn.Parameter(torch.rand((self.unif_dim, self.head_num, self.head_dim), device = self.device)) 
         self.ov_p = nn.Parameter(torch.rand((self.head_num, self.head_dim, self.unif_dim), device = self.device)) 
+
+        self.qvf_p = nn.Linear(self.unif_dim, self.unif_dim, device = self.device)
+        self.kvf_p = nn.Linear(self.unif_dim, self.unif_dim, device = self.device)
         
         self.vec_1, self.vec_f = (nn.Parameter(torch.rand(self.unif_dim)), nn.Parameter(torch.rand(self.unif_dim)))
 
     def MaskAtt(self, compat, route, objectives):
         exclude_l = route.T.clone().detach()
-
-        if torch.any(exclude_l > 0):
-            for i in range(1,exclude_l.shape[1]):
-                compat[self.batch_arr,exclude_l[:,i]] = -float('inf')
+        
+        # allow already traversed paths
+        #if torch.any(exclude_l > 0):
+        #    for i in range(1,exclude_l.shape[1]):
+        #        compat[self.batch_arr,exclude_l[:,i]] = -float('inf')
         
         for i in range(objectives.shape[0]):
             if not (route == objectives[i]).any():
@@ -103,8 +107,9 @@ class PathModule(CommonModule):
         route[torch.all(compat == -float('inf'), dim = 1), 0] = 1
         return route
 
-    def forward(self, hvec, hbar, baseline = False):
+    def forward(self, hvec, hbar, objectives, baseline = False):
         route = torch.zeros(1, self.batch_dim , dtype = torch.int, device = self.device)
+        final_log_prob = torch.zeros(self.batch_dim)
         # run decoder
         for t in range(self.node_dim):
             if route.shape[0] == 1:
@@ -113,31 +118,38 @@ class PathModule(CommonModule):
                 vec_concat = torch.concat((hbar, hvec[route[t-1]], hvec[route[1]]))
 
             qv, kv, vv = ( 
-            torch.einsum('kij,ak->aij', self.qv_p, vec_concat), # batch dim x head num x head dim
-            torch.einsum('kij,abk->abij', self.kv_p, hvec), # batch dim x node dim x head num x head dim
-            torch.einsum('kij,abk->abij', self.vv_p, hvec))
+            torch.einsum('xij,ax->aij', self.qv_p, vec_concat), # batch dim x head num x head dim
+            torch.einsum('xij,abx->abij', self.kv_p, hvec), # batch dim x node dim x head num x head dim
+            torch.einsum('xij,abx->abij', self.vv_p, hvec))
 
-            compat = self.clipp * torch.tanh(torch.einsum('aij,abij->aib',qv,kv)/np.sqrt(self.head_dim)) # batch dim x head num x node dim
-            compat_mask = self.MaskAtt(compat)
-            weight = F.softmax(compat_mask, dim = 1) # weight : batch dim x head num x node dim
+            compat = torch.einsum('abx,aibx->aib',qv,kv)/np.sqrt(self.head_dim) # batch dim x node dim x head num
+            compat_mask = self.MaskAtt(compat, route, objectives)
+            weight = F.softmax(compat_mask, dim = 1) # weight : batch dim x node dim x head num
             # (c) x node dim  X   
-            received_vec = torch.einsum('aib,ajkb->aikb', weight, vv) # recvec : batch dim x head num x head dim
-            mha = torch.einsum('ijk,alij->alk', Wo, received_vec) # mha : batch dim x node dim x unif dim
+            received_vec = torch.einsum('axb,axbi->abi', weight, vv) # recvec : batch dim x head num x head dim
+            hvec_c = torch.einsum('xyj,ixy->ij', self.ov_p, received_vec) # hvec_c : batch dim x unif dim
 
+            qvf = self.qvf_p(hvec_c) # qvf : batch dim x head dim = unif dim (1 head)
+            kvf = self.kvf_p(hvec) # hvf : batch dim x node dim x head dim = unif dim
+
+            compatf = self.clipp * torch.tanh(torch.einsum('ax,aix->ai', qvf, kvf)/np.sqrt(self.unif_dim)) # compatf : batch dim x node dim
+            compatf_mask = self.MaskAtt(compatf, route, objectives)
             
+            policy = F.softmax(compatf_mask, dim = 1)
             
             if baseline:
-                a_t = torch.max(policy, 1).indices
+                target = torch.max(policy, 1).indices
             else:
-                a_t = torch.multinomial(policy, 1).view(self.batch_dim)
-            pi_t = torch.cat((pi_t, a_t.unsqueeze(0)), dim = 0)
-            if torch.all(a_t == 0):
+                target = torch.multinomial(policy, 1).view(self.batch_dim)
+
+            route = torch.cat((route, target.unsqueeze(0)), dim = 0)
+            if torch.all(target == 0):
                 break
         
-            log_prob = torch.log(policy[self.batch_arr, a_t])
+            log_prob = torch.log(policy[self.batch_arr, target]) # value of policy at each target[i]
             final_log_prob += log_prob
 
-        return pi_t, final_log_prob
+        return route, final_log_prob
 
 class TPPModel(nn.Module):
     def __init__(self, encoder, decoder):
@@ -148,7 +160,6 @@ class TPPModel(nn.Module):
 
     def forward(self, adj_matr, objectives, baseline = False):
         hvec, hbar = self.encoder(adj_matr)
-        pi_t, final_log_prob = self.decoder(hvec, hbar, baseline)
+        route, final_log_prob = self.decoder(hvec, hbar, objectives, baseline)
 
-
-        return pi_t.T, final_log_prob
+        return route.T, final_log_prob
